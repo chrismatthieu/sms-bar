a1b3ce14ba9019d8a5531bbb54791432ea58f922
Chris Matthieu <chris@matthieu.us>
2011-06-14 18:15:15 -0700

    added go back link

--- smsbar.rb
+++ smsbar.rb
@@ -3,7 +3,7 @@ require "sinatra"
 require "smsified"
 
 get '/' do
-  '<html><head><style type="text/css"> body { background: url("/speech-bubble.jpg"); background-repeat:no-repeat; background-color: #124D9D; margin: 100 100px; font-size:large; }</style></head><body><h1>Welcome to the SMS Bar!</h1><p>You can now send SMS messages from your web browser\'s address bar using the following format:</p><p>http://smsbar.heroku.com/to_mobile_number/encoded+message</p><p><a href="\">Go back</a></p><br/><p>Powered by <br/><a href="http://smsified.com"><img src="/header-logo.png"></a></p></body></html>'
+  '<html><head><style type="text/css"> body { background: url("/speech-bubble.jpg"); background-repeat:no-repeat; background-color: #124D9D; margin: 100 100px; font-size:large; }</style></head><body><h1>Welcome to the SMS Bar!</h1><p>You can now send SMS messages from your web browser\'s address bar using the following format:</p><p>http://smsbar.heroku.com/to_mobile_number/encoded+message</p><br/><p>Powered by <br/><a href="http://smsified.com"><img src="/header-logo.png"></a></p></body></html>'
 end
 
 get '/:username/:password/:from/:to/:message' do
